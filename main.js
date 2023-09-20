47ce77d1b5f7f3444713b22b929f549dccda085a
Maialinda3007 <108538836+Maialinda3007@users.noreply.github.com>
2023-09-20 12:25:33 -0300

    s

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-
+//pom
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
   }
   
   document.querySelector('.tecla_pom').onclick = tocaSomPom;
+  
+  //tecla clap
+  function tocaSomclap () {
+    document.querySelector('#som_tecla_clap').play();
+  }
+  
+  document.querySelector('.tecla_clap').onclick = tocaSomclap;
+
+  //tecla tim
+  function tocaSomtim () {
+    document.querySelector('#som_tecla_tim').play();
+  }
+  
+  document.querySelector('.tecla_tim').onclick = tocaSomtim;
   
