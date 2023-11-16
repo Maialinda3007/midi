06d231df6fe48474d79cf255eaf3648c0b03d554
Maialinda3007 <108538836+Maialinda3007@users.noreply.github.com>
2023-11-16 15:10:37 -0300

    s

--- main.js
+++ main.js
@@ -1,99 +1,42 @@
-/*O método query-Selector busca apenas o primeiro elemento correspondente ao seletor. Exemplo abaixo*/
-//tecla Pom
-/*function tocaSomPom () {
-  document.querySelector('#som_tecla_pom').play();
-}
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 
-//tecla clap
-function tocaSomClap () {
-  document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-//tecla tim
-function tocaSomTim () {
-  document.querySelector('#som_tecla_tim').play();
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-
-//tecla puff
-function tocaSomPuff () {
-  document.querySelector('#som_tecla_puff').play();
-}
-
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-//tecla splash
-function tocaSomSplash () {
-  document.querySelector('#som_tecla_splash').play();
-}
-
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-
-//tecla toim
-function tocaSomToim () {
-  document.querySelector('#som_tecla_toim').play();
-}
-
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
-
-//tecla psh
-function tocaSomPsh () {
-  document.querySelector('#som_tecla_psh').play();
-}
-
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-
-//tecla tic
-function tocaSomTic () {
-  document.querySelector('#som_tecla_tic').play();
-}
-
-document.querySelector('.tecla_tic').onclick = tocaSomTic;
-
-//tecla tom
-function tocaSomTom () {
-  document.querySelector('#som_tecla_tom').play();
-}
-
-document.querySelector('.tecla_tom').onclick = tocaSomTom;
-*/
-
-
-//-------------------------------------------------------------------------
-
-/*criando referencia - (reduzindo referencia da linha)deve utilizar a palavra reserva-
-da const. A palavra const vem de “constante”, ou seja, algo que nunca muda.
-Então criar um nome para essa lista(para guardar a lista de botoes)
-Para armazenar um valor na referência criada, utilize o comando de atribuição =.
-Conclusão:a lista de elementos encontrada por meio do método de busca
-querySelectorAll será armazenada na referência listaDeTeclas.*/
-
-//const listaDeTeclas = document.querySelectorAll('.tecla');
-
-//listaDeTeclas[0].onclick = tocaSomPom;
-
-//Declarar e chamar a função, executar o som através do play
-/*
-querySelector para selecionar elementos
-concede o acesso ao código, atribuir para tocar o som*/
-//-----------------------------------------------------------------------------
-//Estrutura de repetição  -enquanto - receber se verdadeiro ou falso
-
-function tocaSomPom () {
-  document.querySelector('#som_tecla_pom').play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0; 
-while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onclick = tocaSomPom;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-  contador = contador + 1; 
-  console.log(contador);
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+ //eventos do teclado onkeydowne onkeyup.
+ //adicionar e remover classes em um elemento HTML através do JavaScript, com as funções add e remove do classList.   
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
+
